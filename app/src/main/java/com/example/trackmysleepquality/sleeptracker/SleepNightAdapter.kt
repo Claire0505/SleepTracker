e3eae0adf55f746690e6c60fb83c947754153b5d
Claire <a0505kimo@hotmail>
2021-05-28 14:03:08 +0800

    1.建立新的佈局list_item_sleep_night，用來顯示RecyclerView的itemView 2.打開SleepNightAdapter.kt，建立新的ViewHolder，將原來的TextItemViewHolder取代掉
    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){}
    https://developer.android.com/codelabs/kotlin-android-training-recyclerview-fundamentals#4
    
    3.打開Util.kt，建立新的convertDurationToFormatted()

--- app/src/main/java/com/example/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -2,14 +2,15 @@ package com.example.trackmysleepquality.sleeptracker
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.trackmysleepquality.R
-import com.example.trackmysleepquality.TextItemViewHolder
+import com.example.trackmysleepquality.*
 import com.example.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
     // 創建一個listOf SleepNight變量來保存數據。
     // 要知道RecyclerView何時顯示的數據已更改，請將自定義設置器添加到類data頂部的變量中SleepNightAdapter。
     var data = listOf<SleepNight>()
@@ -19,25 +20,40 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
             notifyDataSetChanged()
         }
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.sleepQuality.toString()
-        // 如果睡眠質量保持在小於或等於1且表示睡眠不良的視圖持有人中，將文本顏色設置為紅色。
-        if (item.sleepQuality <= 1){
-            holder.textView.setTextColor(Color.RED)
-        } else {
-            holder.textView.setTextColor(Color.DKGRAY)
-        }
+        val res = holder.itemView.context.resources
+        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
+        holder.qualityImage.setImageResource( when (item.sleepQuality){
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
     }
 
     override fun getItemCount(): Int {
         return data.size
+    }
+
+    /**
+     * 建立ViewHolder 在內部ViewHolder，獲取對視圖的引用。
+     * 每次綁定時ViewHolder，都需要訪問圖像和兩個文本視圖。（您可以將此代碼轉換為以後使用數據綁定。）
+     */
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val sleepLength : TextView = itemView.findViewById(R.id.sleep_tv_length)
+        val quality : TextView = itemView.findViewById(R.id.quality_tv_string)
+        val qualityImage : ImageView = itemView.findViewById(R.id.quality_image)
+
     }
 }
