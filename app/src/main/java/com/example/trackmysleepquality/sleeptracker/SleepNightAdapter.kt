95c38a8ed859b1d87d0a50b11e676a3feaf872f3
Claire <a0505kimo@hotmail>
2021-05-28 15:18:08 +0800

    1.重構onBindViewHolder（） Right-click, then select Refactor > Extract > Function. 2.Refactor onCreateViewHolder
    https://developer.android.com/codelabs/kotlin-android-training-recyclerview-fundamentals#5

--- app/src/main/java/com/example/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -21,25 +21,12 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
-        holder.qualityImage.setImageResource( when (item.sleepQuality){
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int {
@@ -48,12 +35,41 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     /**
      * 建立ViewHolder 在內部ViewHolder，獲取對視圖的引用。
-     * 每次綁定時ViewHolder，都需要訪問圖像和兩個文本視圖。（您可以將此代碼轉換為以後使用數據綁定。）
+     * 每次綁定時ViewHolder，都需要訪問圖像和兩個文本視圖。
+     * 更改private constructor，以使構造函數是私有的。因為from()現在是一個返回新ViewHolder實例的方法，
+     * 所以沒有任何人可以再調用它的構造函數ViewHolder了。
      */
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView : View) : RecyclerView.ViewHolder(itemView){
         val sleepLength : TextView = itemView.findViewById(R.id.sleep_tv_length)
         val quality : TextView = itemView.findViewById(R.id.quality_tv_string)
         val qualityImage : ImageView = itemView.findViewById(R.id.quality_image)
 
+        fun bind(item: SleepNight) {
+            val res = itemView.context.resources
+            sleepLength.text =
+                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            quality.text = convertNumericQualityToString(item.sleepQuality, res)
+            qualityImage.setImageResource(
+                when (item.sleepQuality) {
+                    0 -> R.drawable.ic_sleep_0
+                    1 -> R.drawable.ic_sleep_1
+                    2 -> R.drawable.ic_sleep_2
+                    3 -> R.drawable.ic_sleep_3
+                    4 -> R.drawable.ic_sleep_4
+                    5 -> R.drawable.ic_sleep_5
+                    else -> R.drawable.ic_sleep_active
+                }
+            )
+        }
+
+        companion object {
+             fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
+                return ViewHolder(view)
+            }
+        }
+
     }
+
 }
